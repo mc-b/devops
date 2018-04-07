faf2be3b19757978eb0d7a1d179b74c495842a69
mc-b <marcel.bernet@ch-open.ch>
2018-04-07 13:17:17 +0200

    localhost:8001/ui ist obsolet

--- kubernetes/dashboard.bat
+++ kubernetes/dashboard.bat
@@ -7,5 +7,5 @@ set DOCKER_TLS_VERIFY=1
 set DOCKER_CERT_PATH=%~d0%~p0.docker
 set KUBECONFIG=%~d0%~p0.kube\config
  
-start http://localhost:8001/ui /B
+start http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/. /B
 kubectl proxy
